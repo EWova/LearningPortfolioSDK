d1d7f788dea138f6ecd7df4443397fc29634ecd8
WenHan <willy870719@gmail.com>
2026-07-07 16:15:54 +0800

    update

--- Assets/EWova.LearningPortfolioSDK/Runtime/LearningPortfolioEWovaAuth.cs
+++ Assets/EWova.LearningPortfolioSDK/Runtime/LearningPortfolioEWovaAuth.cs
@@ -6,13 +6,12 @@ namespace EWova.LearningPortfolio
 {
     public class LearningPortfolioEWovaAuth : AuthProvider
     {
-        public static Logger InternalLogger = new Logger("[EWova]LPEWovaAuth ", LogLevel.Full);
         internal LearningPortfolioEWovaAuth()
             : base(EWovaAuthConfigFactory.Create(options =>
         {
             options.ClientId = "learning-portfolio-sdk";
             options.Scopes = new List<string> { "openid", "profile", "email", "roles", "organization", "offline_access" };
-        }), InternalLogger)
+        }), new Logger("[EWova]LPEWovaAuth ", LogLevel.Full))
         { }
     }
 }
