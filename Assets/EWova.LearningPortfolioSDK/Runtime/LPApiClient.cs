777b7ac2e548ee110ade7b40ac878f5c35c2b224
WenHan <willy870719@gmail.com>
2026-08-13 19:30:47 +0800

    交換測試 正式環境

--- Assets/EWova.LearningPortfolioSDK/Runtime/LPApiClient.cs
+++ Assets/EWova.LearningPortfolioSDK/Runtime/LPApiClient.cs
@@ -19,13 +19,13 @@ namespace EWova.LearningPortfolio
             {
                 if (Environment.DeploymentMode is DeploymentMode.Development)
                 {
+#if UNITY_EDITOR
+                    Authoring.EditorLogger.Warn("你正在編輯器中使用測試環境的 API URL，請確認是否有意這麼做。可到 EWova/Editor/DeveloymentMode 切換回 Production 開發環境。");
+#endif
                     return "https://api-learning-app.ewova.dev/";
                 }
                 else
                 {
-#if UNITY_EDITOR
-                    Authoring.EditorLogger.Warn("你正在編輯器中使用正式環境的 API URL，請確認是否有意這麼做，避免對正式環境造成不必要的影響。可到 EWova/Editor/DeveloymentMode 切換回 Development 開發環境。");
-#endif
                     return "https://api-learning-app.ewova.com/";
                 }
             }
